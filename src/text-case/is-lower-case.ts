e32704edcc4fe422c0ae74b62d344128b40ade63
Dobrak <vigihdev@gmail.com>
2022-12-04 14:39:56 +0700

    commit

--- src/text-case/is-lower-case.ts
+++ src/text-case/is-lower-case.ts
@@ -1,5 +1,5 @@
 /**
- * Returns a boolean indicating whether text is lower case.
+ * Returns a boolean indicating whether text is lower case
  */
 export function isLowerCase(input: string) {
   return input.toLowerCase() === input && input.toUpperCase() !== input;
